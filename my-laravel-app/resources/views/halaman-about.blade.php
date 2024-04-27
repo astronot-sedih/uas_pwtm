75016454d6e98fc650ba10e553abe7c81a134e4b
Ananda Rafli Juliansyah <85611673+astronot-sedih@users.noreply.github.com>
2024-04-27 03:03:28 +0000

    [Add] Add Navbar

--- my-laravel-app/resources/views/halaman-about.blade.php
+++ my-laravel-app/resources/views/halaman-about.blade.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman About</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-link" href="/">Home</a>
+            <a class="nav-link active" aria-current="page" href="/about">About</a>
+            </div>
+        </div>
+        </div>
+    </nav>
     <h1>Halaman About</h1>
 
     <p>NIM: {{ $data['nim'] }}</p>
--- my-laravel-app/resources/views/halaman-home.blade.php
+++ my-laravel-app/resources/views/halaman-home.blade.php
@@ -4,12 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Home</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <a class="nav-link" href="/about">About</a>
+            </div>
+        </div>
+        </div>
+    </nav>
     <h1>Halaman Home</h1>
 
     @foreach($data as $artikel)
         <h2>{{ $artikel['judul_artikel'] }}</h2>
+        <a href="{{ $artikel['sumber'] }}" target="_blank"><h6>Sumber : {{ $artikel['sumber'] }}</h6></a>
         <p>{{ $artikel['isi_artikel'] }}</p>
     @endforeach
 </body>
